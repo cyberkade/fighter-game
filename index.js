ae8edea3c765dbe62e583fda4e3f9987ca235439
Kade Griffith <kadeg95@gmail.com>
2022-04-01 12:11:48 -0600

    adds velocity and gravity logic to the players. players can move around smoothly and jump with gravity affecting the fall

--- index.js
+++ index.js
@@ -6,31 +6,103 @@ canvas.height = 576;
 
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
   }
 
   draw() {
     context.fillStyle = "purple";
-    context.fillRect(this.position.x, this.position.y, 50, 150);
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
   }
 }
 
 const player = new Sprite({
   position: { x: 0, y: 0 },
-  velocity: { x: 0, y: 0 },
+  velocity: { x: 0, y: 10 },
 });
 
 const enemy = new Sprite({
   position: { x: 900, y: 50 },
-  velocity: { x: 0, y: 0 },
+  velocity: { x: 0, y: 10 },
 });
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
+
+let lastKey;
 
 function animate() {
   window.requestAnimationFrame(animate);
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+
+  player.velocity.x = 0;
+  if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1;
+  }
 }
 
-player.draw();
-enemy.draw();
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
